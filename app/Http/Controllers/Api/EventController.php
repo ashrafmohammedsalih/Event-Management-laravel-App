d3c9894baabe0f7d64e3beb15f8428fe76ebc583
ashrafmohammedsalih <ashrafal3oni@gmail.com>
2023-07-30 13:44:55 +0300

    sanctum commit

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -1,18 +1,37 @@
 <?php
 
 namespace App\Http\Controllers\Api;
-
+use App\Http\Resources\EventResource;
 use App\Http\Controllers\Controller;
+use App\Http\Traits\CanLoadRelationships;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
+
+    use CanLoadRelationships;
+    private array $relations = ['user', 'attendees', 'attendees.user'];
+
+
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except(['index', 'show']);
+    }
+
+
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-      return \App\Models\Event::all();
+        
+        $query = $this->loadRelationships(Event::query());
+
+        return EventResource::collection(
+            $query->latest()->paginate()
+        );
     }
 
     /**
@@ -20,30 +39,51 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $event = Event::create([
+            ...$request->validate([
+                'name' => 'required|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'required|date',
+                'end_time' => 'required|date|after:start_time'
+            ]),
+            'user_id' => $request->user()->id
+        ]);
+
+        return new EventResource($this->loadRelationships($event));
     }
 
     /**
      * Display the specified resource.
      */
-    public function show( \App\Models\Event $event)
+    public function show( Event $event)
     {
-        return $event;
+        return new EventResource($this->loadRelationships($event));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Event $event)
     {
-        //
+        $event->update(
+            $request->validate([
+                'name' => 'sometimes|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'sometimes|date',
+                'end_time' => 'sometimes|date|after:start_time'
+            ])
+        );
+
+        return new EventResource($this->loadRelationships($event));
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Event $event)
     {
-        //
+        $event->delete();
+
+        return response(content:"deleted succsessfuly", status:204);
     }
 }
